c31e0bb9c3dc92a50ce6342a942ed12de0173e58
Andrew <zaharowanrew@gmail.com>
2024-12-01 14:29:50 +0400

    fix: cart logic

--- web/app/catalogs/middleweare.py
+++ web/app/catalogs/middleweare.py
@@ -1,29 +1,21 @@
-from django.utils.deprecation import MiddlewareMixin
-from django.core.handlers.wsgi import WSGIRequest
-from catalogs.models import Cart
+from django.contrib.sessions.middleware import MiddlewareMixin
+from django.http import HttpRequest
+from .models import Cart
 
 class CartMiddleware(MiddlewareMixin):
-    def process_request(self, request:WSGIRequest):
-
-
+    def process_request(self, request: HttpRequest):
         if request.user.is_authenticated:
-            # Получаем или создаем корзину для авторизованного пользователя
             cart, created = Cart.objects.get_or_create(user=request.user)
         else:
-            # Для анонимных пользователей используем сессию
             cart_id = request.session.get('cart_id')
             if cart_id:
-                # Если корзина уже есть в сессии
                 try:
                     cart = Cart.objects.get(id=cart_id)
                 except Cart.DoesNotExist:
-                    # Если корзина по id не найдена, создаем новую
                     cart = Cart.objects.create()
                     request.session['cart_id'] = cart.id
             else:
-                # Создаем новую корзину и сохраняем её в сессии
                 cart = Cart.objects.create()
                 request.session['cart_id'] = cart.id
         
-        # Сохраняем корзину в запросе для доступа в представлениях
         request.cart = cart
